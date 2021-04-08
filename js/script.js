fbb45da673c469af54c57cb9fb68c9830e1632ad
lorenzotade <lorenzo.tade@gmail.com>
2021-04-08 11:00:12 +0200

    correzione minima

--- js/script.js
+++ js/script.js
@@ -9,15 +9,15 @@ var total_fare = 0;
 var message = "";
 
 if (age < 18) {
-  fare_km = fare_km - ((fare_km * 20) / 100);
+  fare_km -= fare_km * 0.2;
   total_fare = km * fare_km;
   message = "Il costo del biglietto è di €" + total_fare.toFixed(2) + " avendo applicato uno sconto minorenni del 20%.";
 } else if (age > 65) {
-  fare_km = fare_km - ((fare_km * 40) / 100);
+  fare_km -= fare_km * 0.4;
   total_fare = km * fare_km;
   message = "Il costo del biglietto è di €" + total_fare.toFixed(2) + " avendo applicato uno sconto senior del 40%.";
 } else {
-  total_fare = fare_km * km;
+  total_fare = km * fare_km;
   message = "Il costo del biglietto è di €" + total_fare + ".";
 }
 
